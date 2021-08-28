768498528a9f100685d807ecc25eb9c190f47549
moos <herget06@gmail.com>
2021-08-28 12:44:28 -0400

    models > user.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :players
   resources :teams
+  get '/teams/:team_id/players/:id', to: 'players#add_to_team'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
