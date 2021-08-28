287c5af3b481861fef147d3ae8274cd8b0b104b5
moos <herget06@gmail.com>
2021-08-28 12:31:12 -0400

    players_controller.rb

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :set_player, only: [:show, :update, :destroy]
+  before_action :set_player, only: :add_to_team
 
   # GET /players
   def index
@@ -9,33 +9,11 @@ class PlayersController < ApplicationController
   end
 
   # GET /players/1
-  def show
-    render json: @player
-  end
+  def add_to_team 
+    @player = Team.find(params[:team_id])
+    @player.teams << @flavor 
 
-  # POST /players
-  def create
-    @player = Player.new(player_params)
-
-    if @player.save
-      render json: @player, status: :created, location: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /players/1
-  def update
-    if @player.update(player_params)
-      render json: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /players/1
-  def destroy
-    @player.destroy
+    render json: @team, include: :players
   end
 
   private
@@ -43,9 +21,4 @@ class PlayersController < ApplicationController
     def set_player
       @player = Player.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
-    def player_params
-      params.require(:player).permit(:name, :team_name, :position, :created_at, :updated_at)
-    end
-end
+  end
