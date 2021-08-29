ff5389dedd037f70d3947943b6013bf2d9c32c7a
moos <herget06@gmail.com>
2021-08-29 11:12:16 -0400

    teams.js done

---
+++ client/src/services/teams.js
@@ -0,0 +1,21 @@
+import api from './api-config';
+
+export const getAllTeams = async () => {
+  const res = await api.get('/teams');
+  return res.data;
+};
+
+export const getOneTeam = async (id) => {
+  const res = await api.get(`/teams/${id}`);
+  return res.data;
+};
+
+export const postTeam = async (teamData) => {
+  const res = await api.post('/teams', { team: teamData });
+  return res.data;
+};
+
+export const putTeam = async (id, teamData) => {
+  const res = await api.put(`/teams/${id}`, { team: teamData });
+  return res.data;
+};
