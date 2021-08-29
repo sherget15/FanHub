10a39d7352e4e014025fe62200253d06c12545c5
moos <herget06@gmail.com>
2021-08-29 11:25:50 -0400

    app.js done

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,63 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
+import MainContainer from './containers/MainContainer';
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+  
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
+        <Switch>
+          <Route path='/login'>
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path='/register'>
+            <Register handleRegister={handleRegister} />
+          </Route>
+          <Route path='/'>
+            <MainContainer currentUser={currentUser} />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
