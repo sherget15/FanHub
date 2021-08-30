09007ef7011adafae20a08de05461153473473da
moos <herget06@gmail.com>
2021-08-30 16:24:11 -0400

    maincontainer progress

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,3 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
-useState
+import { getAllTeams, postTeam, putTeam, deleteTeam } from '../services/teams';
+import { getAllPlayers } from '../services/players';
+import Players from '../screens/Players';
+import Teams from '../screens/Teams';
+import TeamCreate from '../screens/TeamCreate';
+import TeamEdit from '../screens/TeamEdit';
+import TeamDetail from '../screens/TeamDetail';
+
+export default function MainContainer(props) {
+  const [teams, setTeams] = useState([]);
+  const [players, setPlayers] = useState([]);
+  const { currentUser } = props;
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const teamList = await getAllTeams();
+      setTeams(teamList);
+    };
+    fetchTeams();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const playerList = await getAllPlayers();
+      setPlayers(playerList);
+    };
+    fetchPlayers();
+  }, []);
+
+  const handleCreate = async (formData) => {
+    const teamData = await postTeam(formData);
+    setTeams((prevState) => [...prevState, teamData]);
+    history.push('/teams');
+  };
+
+  const handleUpdate = async (id, formData) => {
+    const teamData = await putTeam(id, formData);
+    setTeams((prevState) =>
+      prevState.map((team) => {
+        return team.id === Number(id) ? teamData : team;
+      })
+    );
+    history.push('/teams');
+  };
+}
