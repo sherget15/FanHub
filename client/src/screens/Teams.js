1ef576b5b32ce2e33ce5b42e1a99886f10e20505
moos <herget06@gmail.com>
2021-08-31 07:07:06 -0400

    teams screen

---
+++ client/src/screens/Teams.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function Teams(props) {
+  const { teams, handleDelete, currentUser } = props;
+
+  return (
+    <div>
+      <h3>Teams</h3>
+      {teams.map((team) => (
+        <div key={team.id}>
+          <Link to={`/teams/${team.id}`}>
+            <p>{team.name}</p>
+          </Link>
+          {currentUser?.id === team.user_id && (
+            <div>
+              <Link to={`/teams/${team.id}/edit`}>
+                <button>Edit</button>
+              </Link>
+              <button onClick={() => handleDelete(team.id)}>Delete</button>
+            </div>
+          )}
+        </div>
+      ))}
+      <Link to='/teams/new'><button>Create</button></Link>
+    </div>
+  );
+}
