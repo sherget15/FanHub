3aa32fa5b71d41c348c0ce07783b27f2ff356269
moos <herget06@gmail.com>
2021-08-31 07:23:53 -0400

    team edit screen

---
+++ client/src/screens/TeamEdit.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import {useParams} from 'react-router-dom';
+
+export default function TeamEdit(props) {
+  const [formData, setFormData] = useState({
+    name: ''
+  })
+  const {teams, handleUpdate} = props;
+  const {id} = useParams();
+
+  useEffect(() => {
+    const prefillFormData = () => {
+      const teamItem = teams.find((team) => team.id === Number(id));
+      setFormData({ name: teamItem.name })
+    }
+    if (teams.length) {
+      prefillFormData()
+    }
+  }, [teams, id])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  return (
+    <form
+    onSubmit={(e) => {
+				e.preventDefault();
+				handleUpdate(id, formData);
+			}}
+      >
+      <label>
+        Name:
+        <input 
+          type='text' 
+          name='name' 
+          value={formData.name} 
+          onChange={handleChange}
+        />
+      </label>
+      <button>Submit</button>
+    </form>
+  )
+
+}
