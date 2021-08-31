9ef81899ec04b2ee2a5d6d8c43c5afaf1fde845a
moos <herget06@gmail.com>
2021-08-31 10:59:44 -0400

    heroku address in config

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //
-const baseUrl = process.env.NODE_ENV === 'production' ? 'deployed link goes here' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://fanhub-api.herokuapp.com/' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl,  
