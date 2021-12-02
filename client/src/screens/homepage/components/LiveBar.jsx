eff3634f7dcd3d4fe52990425324c5077f0a1ebf
moos <herget06@gmail.com>
2021-12-02 16:12:47 -0500

    live bar - css changes

--- client/src/screens/homepage/components/LiveBar.jsx
+++ client/src/screens/homepage/components/LiveBar.jsx
@@ -3,14 +3,34 @@ import './LiveBar.css'
 
 export default function LiveBar() {
   return (
-    <div>
-      <div>
+    <div className='liveBar'>
 
-        <div>
+      <div className='mlbContainer'>
+
+        <div className='mlbScoresCont'>
+          <div className='mlb1'>
+            <p>StL - 1</p>
+            <p>ATL - 9</p>
+          </div>
+          <div className='mlb2'>
+            <p>StL - 1</p>
+            <p>ATL - 9</p>
+          </div>
+          <div className='mlb3'>
+            <p>StL - 1</p>
+            <p>ATL - 9</p>
+          </div>
+        </div>
+
+        <div className='mlbHeader'>
         <p>MLB</p>
         </div>
+      
+      </div>
 
-        <div>
+      <div className='nflContainer'>
+
+        <div className='nflScoresCont'>
           <div>
             <p>StL - 1</p>
             <p>ATL - 9</p>
@@ -24,15 +44,16 @@ export default function LiveBar() {
             <p>ATL - 9</p>
           </div>
         </div>
-      
-      </div>
-      <div>
-        
-        <div>
+
+        <div className='nflHeader'>
         <p>NFL</p>
         </div>
+      
+      </div>
 
-        <div>
+      <div className='mlsContainer'>
+
+        <div className='mlsScoresCont'>
           <div>
             <p>StL - 1</p>
             <p>ATL - 9</p>
@@ -46,15 +67,16 @@ export default function LiveBar() {
             <p>ATL - 9</p>
           </div>
         </div>
-      
-      </div>
-      <div>
 
-        <div>
+        <div className='mlsHeader'>
         <p>MLS</p>
         </div>
+      
+      </div>
 
-        <div>
+      <div className='nbaContainer'>
+
+        <div className='nbaScoresCont'>
           <div>
             <p>StL - 1</p>
             <p>ATL - 9</p>
@@ -68,31 +90,13 @@ export default function LiveBar() {
             <p>ATL - 9</p>
           </div>
         </div>
-      
-      </div>
 
-      <div>
-
-        <div>
+        <div className='nbaHeader'>
         <p>NBA</p>
         </div>
 
-        <div>
-          <div>
-            <p>StL - 1</p>
-            <p>ATL - 9</p>
-          </div>
-          <div>
-            <p>StL - 1</p>
-            <p>ATL - 9</p>
-          </div>
-          <div>
-            <p>StL - 1</p>
-            <p>ATL - 9</p>
-          </div>
-        </div>
-        
       </div>
+
     </div>
   )
 }
